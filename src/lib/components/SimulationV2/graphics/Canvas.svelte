1290eb245177344819b73c6bcd0a00fd1e557605
Adrien Fabre <adrien.fabre@dashlane.com>
2023-02-20 23:14:07 +0100

    Compute FPS

--- src/lib/components/SimulationV2/graphics/Canvas.svelte
+++ src/lib/components/SimulationV2/graphics/Canvas.svelte
@@ -8,6 +8,8 @@
     let _p5: p5;
     export let cells: Cell[];
     export let worldSize: WorldSize;
+    let avgFPS: string;
+    const fpsHistory: number[] = [];
 
     const sketch: Sketch = (p5) => {
         p5.setup = () => {
@@ -17,7 +19,16 @@
         p5.draw = () => {
             p5.background(0);
             drawCells(p5, cells, worldSize);
+
+            avgFPS = getFPS(p5);
         };
+    };
+
+    const getFPS = (p: p5) => {
+        fpsHistory.push(p.frameRate());
+        if (fpsHistory.length > 60) fpsHistory.shift();
+        const avgFrameRate = fpsHistory.reduce((avg, r) => avg + r / fpsHistory.length, 0);
+        return avgFrameRate.toFixed(0);
     };
 
     onDestroy(() => {
@@ -27,4 +38,5 @@
 
 <div>
     <P5 {sketch} />
+    <span>FPS: {avgFPS}</span>
 </div>
