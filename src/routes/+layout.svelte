77c7746d98a11e65247069dc6a2fd1475971bae1
Adrien Fabre <adrien.fabre@dashlane.com>
2023-02-26 18:02:42 +0100

    Try to fix web workers build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
+<script>
+    import { base } from '$app/paths';
+</script>
+
 <header>
     <h1>Particles Life</h1>
 
     <nav>
-        <a href="/">Home</a> |
-        <a href="/webgl">WebGL</a> |
-        <a href="/distance">Distance test</a> |
-        <a href="/testWorkers">Workers test</a> |
-        <a href="/v2">V2</a> |
-        <a href="/v3">V3</a> |
+        <a href="{base}/">Home</a> |
+        <a href="{base}/webgl">WebGL</a> |
+        <a href="{base}/distance">Distance test</a> |
+        <a href="{base}/testWorkers">Workers test</a> |
+        <a href="{base}/v2">V2</a> |
+        <a href="{base}/v3">V3</a> |
     </nav>
 </header>
 
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
     plugins: [sveltekit()],
-    test: {}
+    test: {},
+    build: {
+        // This build target is necessary to make top-level await work
+        // and I use top-level await to load workers
+        target: 'esnext'
+    }
 };
 
 export default config;
