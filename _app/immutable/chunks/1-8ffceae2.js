aa9081b6cee439bc55572d4d40bd02bdf60fa801
statox <me@statox.fr>
2023-02-26 17:24:29 +0000

    Deploying to gh-pages from @ statox/particles-life@77c7746d98a11e65247069dc6a2fd1475971bae1 🚀

---
+++ _app/immutable/chunks/1-8ffceae2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-43992cae.js";export{t as component};
---
+++ _app/immutable/components/pages/_layout.svelte-3018ced2.js
@@ -0,0 +1,11 @@
+import{S as X,i as Y,s as Z,C as x,k as h,q as l,a as Q,l as c,m as _,r as n,h as i,c as T,n as V,b as U,D as e,E as ee,F as te,G as ae,f as se,t as re}from"../../chunks/index-689c9b92.js";import{b as W}from"../../chunks/paths-b4419565.js";function le(d){let o,u,k,m,a,p,H,y,v,G,q,E,C,P,A,S,g,$,w,F,b,N,R,D,L;const j=d[1].default,s=x(j,d,d[0],null);return{c(){o=h("header"),u=h("h1"),k=l("Particles Life"),m=Q(),a=h("nav"),p=h("a"),H=l("Home"),y=l(` |
+        `),v=h("a"),G=l("WebGL"),q=l(` |
+        `),E=h("a"),C=l("Distance test"),P=l(` |
+        `),A=h("a"),S=l("Workers test"),g=l(` |
+        `),$=h("a"),w=l("V2"),F=l(` |
+        `),b=h("a"),N=l("V3"),R=l(" |"),D=Q(),s&&s.c(),this.h()},l(t){o=c(t,"HEADER",{});var f=_(o);u=c(f,"H1",{});var z=_(u);k=n(z,"Particles Life"),z.forEach(i),m=T(f),a=c(f,"NAV",{});var r=_(a);p=c(r,"A",{href:!0});var B=_(p);H=n(B,"Home"),B.forEach(i),y=n(r,` |
+        `),v=c(r,"A",{href:!0});var I=_(v);G=n(I,"WebGL"),I.forEach(i),q=n(r,` |
+        `),E=c(r,"A",{href:!0});var J=_(E);C=n(J,"Distance test"),J.forEach(i),P=n(r,` |
+        `),A=c(r,"A",{href:!0});var K=_(A);S=n(K,"Workers test"),K.forEach(i),g=n(r,` |
+        `),$=c(r,"A",{href:!0});var M=_($);w=n(M,"V2"),M.forEach(i),F=n(r,` |
+        `),b=c(r,"A",{href:!0});var O=_(b);N=n(O,"V3"),O.forEach(i),R=n(r," |"),r.forEach(i),f.forEach(i),D=T(t),s&&s.l(t),this.h()},h(){V(p,"href",W+"/"),V(v,"href",W+"/webgl"),V(E,"href",W+"/distance"),V(A,"href",W+"/testWorkers"),V($,"href",W+"/v2"),V(b,"href",W+"/v3")},m(t,f){U(t,o,f),e(o,u),e(u,k),e(o,m),e(o,a),e(a,p),e(p,H),e(a,y),e(a,v),e(v,G),e(a,q),e(a,E),e(E,C),e(a,P),e(a,A),e(A,S),e(a,g),e(a,$),e($,w),e(a,F),e(a,b),e(b,N),e(a,R),U(t,D,f),s&&s.m(t,f),L=!0},p(t,[f]){s&&s.p&&(!L||f&1)&&ee(s,j,t,t[0],L?ae(j,t[0],f,null):te(t[0]),null)},i(t){L||(se(s,t),L=!0)},o(t){re(s,t),L=!1},d(t){t&&i(o),t&&i(D),s&&s.d(t)}}}function ne(d,o,u){let{$$slots:k={},$$scope:m}=o;return d.$$set=a=>{"$$scope"in a&&u(0,m=a.$$scope)},[m,k]}class ie extends X{constructor(o){super(),Y(this,o,ne,le,Z,{})}}export{ie as default};
