f980cbdab59ba76f9d81433df1a470c06cd8f661
Adrien Fabre <adrien.fabre@dashlane.com>
2023-03-06 00:06:46 +0100

    More function extraction

--- src/lib/shaders/components/Particles2.svelte
+++ src/lib/shaders/components/Particles2.svelte
@@ -4,6 +4,13 @@
     import * as m4 from '../m4';
     import { getArrays } from '../simulationUtils';
 
+    type PositionsInfo = {
+        fb: WebGLFramebuffer;
+        tex: WebGLTexture;
+    };
+
+    type ProgLocs = { [name: string]: number | WebGLUniformLocation | null };
+
     function main() {
         const updatePositionVS = `
   attribute vec4 position;
@@ -37,7 +44,7 @@
 
     // compute texcoord from gl_FragCoord;
     vec2 texcoord = gl_FragCoord.xy / texDimensions;
-    
+
     vec2 position = texture2D(positionTex, texcoord).xy;
     //vec2 velocity = texture2D(velocityTex, texcoord).xy;
     vec2 velocity = vec2(0.0, -100);
@@ -160,9 +167,8 @@
             particleTexHeight
         );
 
-        // create 2 framebuffers. One that renders to positionTex1 one
+        // create 2 framebuffers. One that renders to positionTex1
         // and another that renders to positionTex2
-
         const positionsFB1 = webglUtils.createFramebuffer(gl, positionTex1);
         const positionsFB2 = webglUtils.createFramebuffer(gl, positionTex2);
 
@@ -177,80 +183,38 @@
 
         let then = 0;
         function render(time: number) {
-            if (!gl) {
-                return;
+            if (!gl || !updatePositionBuffer || !idBuffer) {
+                throw 'Undefined params to render';
             }
             // convert to seconds
             time *= 0.001;
-            // Subtract the previous time from the current time
             const deltaTime = time - then;
-            // Remember the current time for the next frame.
             then = time;
 
             webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
 
-            // render to the new positions
-            gl.bindFramebuffer(gl.FRAMEBUFFER, newPositionsInfo.fb);
-            gl.viewport(0, 0, particleTexWidth, particleTexHeight);
-
-            // setup our attributes to tell WebGL how to pull
-            // the data from the buffer above to the position attribute
-            // this buffer just contains a -1 to +1 quad for rendering
-            // to every pixel
-            gl.bindBuffer(gl.ARRAY_BUFFER, updatePositionBuffer);
-            gl.enableVertexAttribArray(updatePositionPrgLocs.position);
-            gl.vertexAttribPointer(
-                updatePositionPrgLocs.position,
-                2, // size (num components)
-                gl.FLOAT, // type of data in buffer
-                false, // normalize
-                0, // stride (0 = auto)
-                0 // offset
+            runUpdatePositions(
+                gl,
+                newPositionsInfo,
+                particleTexWidth,
+                particleTexHeight,
+                updatePositionBuffer,
+                updatePositionPrgLocs,
+                oldPositionsInfo,
+                velocityTex,
+                updatePositionProgram,
+                deltaTime
             );
 
-            gl.activeTexture(gl.TEXTURE0);
-            gl.bindTexture(gl.TEXTURE_2D, oldPositionsInfo.tex);
-            gl.activeTexture(gl.TEXTURE0 + 1);
-            gl.bindTexture(gl.TEXTURE_2D, velocityTex);
-
-            gl.useProgram(updatePositionProgram);
-            gl.uniform1i(updatePositionPrgLocs.positionTex, 0); // tell the shader the position texture is on texture unit 0
-            gl.uniform1i(updatePositionPrgLocs.velocityTex, 1); // tell the shader the position texture is on texture unit 1
-            gl.uniform2f(updatePositionPrgLocs.texDimensions, particleTexWidth, particleTexHeight);
-            gl.uniform2f(updatePositionPrgLocs.canvasDimensions, gl.canvas.width, gl.canvas.height);
-            gl.uniform1f(updatePositionPrgLocs.deltaTime, deltaTime);
-
-            gl.drawArrays(gl.TRIANGLES, 0, 6); // draw 2 triangles (6 vertices)
-
-            gl.bindFramebuffer(gl.FRAMEBUFFER, null);
-            gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-            // setup our attributes to tell WebGL how to pull
-            // the data from the buffer above to the id attribute
-            gl.bindBuffer(gl.ARRAY_BUFFER, idBuffer);
-            gl.enableVertexAttribArray(drawParticlesProgLocs.id);
-            gl.vertexAttribPointer(
-                drawParticlesProgLocs.id,
-                1, // size (num components)
-                gl.FLOAT, // type of data in buffer
-                false, // normalize
-                0, // stride (0 = auto)
-                0 // offset
+            runDrawParticles(
+                gl,
+                particleTexWidth,
+                idBuffer,
+                drawParticlesProgLocs,
+                newPositionsInfo,
+                drawParticlesProgram,
+                numParticles
             );
-
-            gl.activeTexture(gl.TEXTURE0);
-            gl.bindTexture(gl.TEXTURE_2D, newPositionsInfo.tex);
-
-            gl.useProgram(drawParticlesProgram);
-            gl.uniform2f(drawParticlesProgLocs.texDimensions, particleTexWidth, particleTexWidth);
-            gl.uniform1i(drawParticlesProgLocs.positionTex, 0); // tell the shader the position texture is on texture unit 0
-            gl.uniformMatrix4fv(
-                drawParticlesProgLocs.matrix,
-                false,
-                m4.orthographic(0, gl.canvas.width, 0, gl.canvas.height, -1, 1)
-            );
-
-            gl.drawArrays(gl.POINTS, 0, numParticles);
 
             // swap which texture we will read from
             // and which one we will write to
@@ -264,6 +228,92 @@
         }
         requestAnimationFrame(render);
     }
+
+    const runDrawParticles = (
+        gl: WebGLRenderingContext,
+        particleTexWidth: number,
+        idBuffer: WebGLBuffer,
+        drawParticlesProgLocs: ProgLocs,
+        newPositionsInfo: PositionsInfo,
+        drawParticlesProgram: WebGLProgram,
+        numParticles: number
+    ) => {
+        gl.bindFramebuffer(gl.FRAMEBUFFER, null);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+        // setup our attributes to tell WebGL how to pull
+        // the data from the buffer above to the id attribute
+        gl.bindBuffer(gl.ARRAY_BUFFER, idBuffer);
+        gl.enableVertexAttribArray(drawParticlesProgLocs.id as number);
+        gl.vertexAttribPointer(
+            drawParticlesProgLocs.id as number,
+            1, // size (num components)
+            gl.FLOAT, // type of data in buffer
+            false, // normalize
+            0, // stride (0 = auto)
+            0 // offset
+        );
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, newPositionsInfo.tex);
+
+        gl.useProgram(drawParticlesProgram);
+        gl.uniform2f(drawParticlesProgLocs.texDimensions, particleTexWidth, particleTexWidth);
+        gl.uniform1i(drawParticlesProgLocs.positionTex, 0); // tell the shader the position texture is on texture unit 0
+        gl.uniformMatrix4fv(
+            drawParticlesProgLocs.matrix,
+            false,
+            m4.orthographic(0, gl.canvas.width, 0, gl.canvas.height, -1, 1)
+        );
+
+        gl.drawArrays(gl.POINTS, 0, numParticles);
+    };
+
+    const runUpdatePositions = (
+        gl: WebGLRenderingContext,
+        newPositionsInfo: PositionsInfo,
+        particleTexWidth: number,
+        particleTexHeight: number,
+        updatePositionBuffer: WebGLBuffer,
+        updatePositionPrgLocs: ProgLocs,
+        oldPositionsInfo: PositionsInfo,
+        velocityTex: WebGLTexture,
+        updatePositionProgram: WebGLProgram,
+        deltaTime: number
+    ) => {
+        // render to the new positions
+        gl.bindFramebuffer(gl.FRAMEBUFFER, newPositionsInfo.fb);
+        gl.viewport(0, 0, particleTexWidth, particleTexHeight);
+
+        // setup our attributes to tell WebGL how to pull
+        // the data from the buffer above to the position attribute
+        // this buffer just contains a -1 to +1 quad for rendering
+        // to every pixel
+        gl.bindBuffer(gl.ARRAY_BUFFER, updatePositionBuffer);
+        gl.enableVertexAttribArray(updatePositionPrgLocs.position as number);
+        gl.vertexAttribPointer(
+            updatePositionPrgLocs.position as number,
+            2, // size (num components)
+            gl.FLOAT, // type of data in buffer
+            false, // normalize
+            0, // stride (0 = auto)
+            0 // offset
+        );
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, oldPositionsInfo.tex);
+        gl.activeTexture(gl.TEXTURE0 + 1);
+        gl.bindTexture(gl.TEXTURE_2D, velocityTex);
+
+        gl.useProgram(updatePositionProgram);
+        gl.uniform1i(updatePositionPrgLocs.positionTex, 0); // tell the shader the position texture is on texture unit 0
+        gl.uniform1i(updatePositionPrgLocs.velocityTex, 1); // tell the shader the position texture is on texture unit 1
+        gl.uniform2f(updatePositionPrgLocs.texDimensions, particleTexWidth, particleTexHeight);
+        gl.uniform2f(updatePositionPrgLocs.canvasDimensions, gl.canvas.width, gl.canvas.height);
+        gl.uniform1f(updatePositionPrgLocs.deltaTime, deltaTime);
+
+        gl.drawArrays(gl.TRIANGLES, 0, 6); // draw 2 triangles (6 vertices)
+    };
 
     onMount(() => main());
 </script>
