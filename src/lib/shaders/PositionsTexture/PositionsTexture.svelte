0ef1b1714d8017dded4b08a674d1f0fa04e18d2c
Adrien Fabre <adrien.fabre@dashlane.com>
2023-03-13 23:54:01 +0100

    Use a texture to store positions to render

--- src/lib/shaders/PositionsTexture/PositionsTexture.svelte
+++ src/lib/shaders/PositionsTexture/PositionsTexture.svelte
@@ -2,13 +2,11 @@
     import { onMount } from 'svelte';
     import * as webglUtils from '../webglUtils';
 
-    import drawPositionsBufferVS from './glsl/drawPositionsBuffer.vert.glsl';
-    import drawPositionsBufferFS from './glsl/drawPositionsBuffer.frag.glsl';
-    import { getInitialPositions } from './simulationUtils';
+    import drawPositionsVS from './glsl/drawPositionsTexture.vert.glsl';
+    import drawPositionsFS from './glsl/drawPositionsTexture.frag.glsl';
 
     const screenWidth = 800;
     const screenHeight = 600;
-    const positions = getInitialPositions(screenWidth, screenHeight);
     function main() {
         // Get A WebGL context
         const canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -21,20 +19,24 @@
         }
         webglUtils.checkWebGlCapabilities(gl);
 
-        const program = webglUtils.createProgramFromSources(gl, [
-            drawPositionsBufferVS,
-            drawPositionsBufferFS
-        ]);
+        const program = webglUtils.createProgramFromSources(gl, [drawPositionsVS, drawPositionsFS]);
 
         webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
 
-        const positionAttributeLocation = gl.getAttribLocation(program, 'a_position');
+        const idAttributeLocation = gl.getAttribLocation(program, 'id');
+        const texDimensionsUniformLocation = gl.getUniformLocation(program, 'texDimensions');
         const resolutionUniformLocation = gl.getUniformLocation(program, 'u_resolution');
 
-        const positionBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+        const idBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, idBuffer);
 
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+        const ids = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(ids), gl.STATIC_DRAW);
+
+        const positions = ids
+            .map((_) => [Math.random() * screenWidth, Math.random() * screenHeight, 0, 0])
+            .flat();
+        let positionTex = webglUtils.createTexture(gl, new Float32Array(positions), 3, 3);
 
         function render() {
             if (!gl) {
@@ -45,7 +47,9 @@
                 positions[i] = (positions[i] + Math.random() * 2 - 1) % gl.canvas.width;
                 positions[i + 1] = (positions[i + 1] + Math.random() * 0 + 1) % gl.canvas.height;
             }
-            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+            positionTex = webglUtils.createTexture(gl, new Float32Array(positions), 3, 3);
+
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(ids), gl.STATIC_DRAW);
 
             webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
             gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
@@ -53,32 +57,29 @@
             gl.clearColor(0, 0, 0, 0);
             gl.clear(gl.COLOR_BUFFER_BIT);
 
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, positionTex);
+
             gl.useProgram(program);
 
             gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
+            gl.uniform2f(texDimensionsUniformLocation, 3, 3);
 
-            gl.enableVertexAttribArray(positionAttributeLocation);
+            gl.enableVertexAttribArray(idAttributeLocation);
 
             // Bind the position buffer.
-            gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+            gl.bindBuffer(gl.ARRAY_BUFFER, idBuffer);
 
             // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-            const size = 2; // 2 components per iteration
+            const size = 1; // 2 components per iteration
             const type = gl.FLOAT; // the data is 32bit floats
             const normalize = false; // don't normalize the data
             const stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
             const offset = 0; // start at the beginning of the buffer
-            gl.vertexAttribPointer(
-                positionAttributeLocation,
-                size,
-                type,
-                normalize,
-                stride,
-                offset
-            );
+            gl.vertexAttribPointer(idAttributeLocation, size, type, normalize, stride, offset);
 
             var primitiveType = gl.POINTS;
-            var count = positions.length / 2;
+            var count = ids.length;
             gl.drawArrays(primitiveType, offset, count);
 
             requestAnimationFrame(render);
@@ -86,18 +87,14 @@
         requestAnimationFrame(render);
     }
 
-    const addPosition = () => {
-        positions.push(Math.random() * screenWidth);
-        positions.push(Math.random() * screenHeight);
-    };
-
     onMount(() => main());
 </script>
 
 <p>
-    Create an array of render-size positions. Bind this array to a buffer in the shader program. In
-    each <code>requestAnimationFrame</code> update the positions randomly with javascript and use the
-    shader program to render each position as a point.
+    Create an array of render-size positions. Store this array in a texture. In each <code>
+        requestAnimationFrame</code
+    > update the positions randomly with javascript and create a new texture holding the positions then
+    use a shader to render the texture.
 </p>
 <canvas id="canvas" style="background-color: black" width={screenWidth} height={screenHeight} />
 
---
+++ src/lib/shaders/PositionsTexture/glsl/drawPositionsTexture.vert.glsl
@@ -0,0 +1,31 @@
+attribute float id;
+uniform sampler2D positionTex;
+uniform vec2 texDimensions;
+
+uniform vec2 u_resolution;
+
+vec2 getValueFrom2DTextureAs1DArray(sampler2D tex, vec2 dimensions, float index) {
+    float y = floor(index / dimensions.x);
+    float x = mod(index, dimensions.x);
+    vec2 texcoord = (vec2(x, y) + 0.5) / dimensions;
+    return texture2D(tex, texcoord).xy;
+}
+
+// all shaders have a main function
+void main() {
+    // pull the position from the texture
+    vec2 position = getValueFrom2DTextureAs1DArray(positionTex, texDimensions, id);
+
+    // convert the position from pixels to 0.0 to 1.0
+    vec2 zeroToOne = position / u_resolution;
+ 
+    // convert from 0->1 to 0->2
+    vec2 zeroToTwo = zeroToOne * 2.0;
+ 
+    // convert from 0->2 to -1->+1 (clip space)
+    vec2 clipSpace = zeroToTwo - 1.0;
+ 
+    // Translate to have 0, 0 on top left
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
+    gl_PointSize = 10.0;
+}
