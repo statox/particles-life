a474897afa92fdc62ba0924a404b5305098fe36a
Adrien Fabre <adrien.fabre@dashlane.com>
2023-04-05 00:50:28 +0200

    Implement game of life in fragment shader

--- src/lib/GameOfLife/updateCells/updateCells.frag.glsl
+++ src/lib/GameOfLife/updateCells/updateCells.frag.glsl
@@ -3,8 +3,7 @@ precision highp float;
 uniform sampler2D uInputTexture;
 uniform vec2 uTextureSize;
 
-vec2 wrapCoord(vec2 coord, vec2 dimension) {
-    vec2 step = vec2(1.0, 1.0) / uTextureSize;
+vec2 wrapCoord(vec2 coord) {
     if (coord.x >= 1.0) {
         coord.x = 0.0;
     }
@@ -23,10 +22,39 @@ vec2 wrapCoord(vec2 coord, vec2 dimension) {
 
 void main() {
     vec2 texcoord = gl_FragCoord.xy / uTextureSize;
-    vec2 neighborcoord = texcoord + (vec2(0.0, -1.0) / uTextureSize);
+    float alive = texture2D(uInputTexture, texcoord).x;
 
-    neighborcoord = wrapCoord(neighborcoord, uTextureSize);
-    float neighborcolor = texture2D(uInputTexture, neighborcoord).x;
+    vec2 topLeftCoord  = wrapCoord(texcoord + (vec2(-1.0, -1.0) / uTextureSize));
+    vec2 topCoord      = wrapCoord(texcoord + (vec2(0.0, -1.0) / uTextureSize));
+    vec2 topRightCoord = wrapCoord(texcoord + (vec2(1.0, -1.0) / uTextureSize));
 
-    gl_FragColor = vec4(neighborcolor, 0.0, 0.0, 0.0);
+    vec2 leftCoord  = wrapCoord(texcoord + (vec2(-1.0, 0.0) / uTextureSize));
+    vec2 rightCoord = wrapCoord(texcoord + (vec2(1.0, 0.0) / uTextureSize));
+
+    vec2 bottomLeftCoord  = wrapCoord(texcoord + (vec2(-1.0, 1.0) / uTextureSize));
+    vec2 bottomCoord      = wrapCoord(texcoord + (vec2(0.0, 1.0) / uTextureSize));
+    vec2 bottomRightCoord = wrapCoord(texcoord + (vec2(1.0, 1.0) / uTextureSize));
+
+    float topLeftAlive  = texture2D(uInputTexture, topLeftCoord ).x;
+    float topAlive      = texture2D(uInputTexture, topCoord     ).x;
+    float topRightAlive = texture2D(uInputTexture, topRightCoord).x;
+
+    float leftAlive  = texture2D(uInputTexture, leftCoord ).x;;
+    float rightAlive = texture2D(uInputTexture, rightCoord).x;
+
+    float bottomLeftAlive  = texture2D(uInputTexture, bottomLeftCoord ).x;
+    float bottomAlive      = texture2D(uInputTexture, bottomCoord     ).x;
+    float bottomRightAlive = texture2D(uInputTexture, bottomRightCoord).x;
+
+    float aliveNeighbors = topLeftAlive + topAlive + topRightAlive + leftAlive + rightAlive + bottomLeftAlive + bottomAlive + bottomRightAlive;
+
+    float nextAlive = 0.0;
+    if (alive == 1.0 && (aliveNeighbors == 2.0 || aliveNeighbors == 3.0)) {
+        nextAlive = 1.0;
+    } else if (alive == 0.0 && aliveNeighbors == 3.0) {
+        nextAlive = 1.0;
+    }
+
+
+    gl_FragColor = vec4(nextAlive, 0.0, 0.0, 0.0);
 }
