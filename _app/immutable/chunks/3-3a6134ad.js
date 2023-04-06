1ad14354ed1094d0a24bd2291fd386c2d99d7c62
statox <me@statox.fr>
2023-04-06 20:56:10 +0000

    Deploying to gh-pages from @ statox/particles-life@3247976984ec5833eb8e53058f1b3aab1251d88c 🚀

---
+++ _app/immutable/chunks/3-3a6134ad.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/gameoflife/_page.svelte-c7035bcf.js";export{t as component};
--- _app/immutable/chunks/3-f832d481.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/gameoflife/_page.svelte-da6d8c91.js";export{t as component};
