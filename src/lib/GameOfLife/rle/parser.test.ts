a0e3c2f30cf5ca4dd4b7cc3bd2f829a025fd8eb1
Adrien Fabre <adrien.fabre@dashlane.com>
2023-04-11 23:59:16 +0200

    Completely rework configuration parsing

--- src/lib/GameOfLife/rle/parser.test.ts
+++ src/lib/GameOfLife/rle/parser.test.ts
@@ -1,8 +1,96 @@
 import { describe, expect, it } from 'vitest';
 import { getConfiguration } from './configurations';
-import { parseConfigurationString } from './parser';
+import {
+    parseConfigurationString,
+    lineToLineItems,
+    lineItemsToTokens,
+    tokensToMatrix
+} from './parser';
 
 import gliderString from './patterns/glider.rle';
+
+describe('parsing pipeline', () => {
+    it('simple pattern', () => {
+        const input = 'ooo$3b$2ob!';
+        const items = lineToLineItems(input);
+        expect(items).toEqual(['o', 'o', 'o', '$', '3', 'b', '$', '2', 'o', 'b', '!']);
+        const tokens = lineItemsToTokens(items);
+        expect(tokens).toEqual([
+            { count: 1, tag: 'o' },
+            { count: 1, tag: 'o' },
+            { count: 1, tag: 'o' },
+            { count: 1, tag: '$' },
+            { count: 3, tag: 'b' },
+            { count: 1, tag: '$' },
+            { count: 2, tag: 'o' },
+            { count: 1, tag: 'b' }
+        ]);
+    });
+    it('simple pattern start with number', () => {
+        const input = '2bo$3b$2ob!';
+        const items = lineToLineItems(input);
+        expect(items).toEqual(['2', 'b', 'o', '$', '3', 'b', '$', '2', 'o', 'b', '!']);
+        const tokens = lineItemsToTokens(items);
+        expect(tokens).toEqual([
+            { count: 2, tag: 'b' },
+            { count: 1, tag: 'o' },
+            { count: 1, tag: '$' },
+            { count: 3, tag: 'b' },
+            { count: 1, tag: '$' },
+            { count: 2, tag: 'o' },
+            { count: 1, tag: 'b' }
+        ]);
+    });
+    it('double digit', () => {
+        const input = 'ooo$33b$2ob!';
+        const items = lineToLineItems(input);
+        expect(items).toEqual(['o', 'o', 'o', '$', '33', 'b', '$', '2', 'o', 'b', '!']);
+    });
+    it('double digit start with number', () => {
+        const input = '123o$33b$2ob!';
+        const items = lineToLineItems(input);
+        expect(items).toEqual(['123', 'o', '$', '33', 'b', '$', '2', 'o', 'b', '!']);
+    });
+
+    it('Ignore what is after !', () => {
+        const input = 'ooo!$3b$2ob hello world';
+        const items = lineToLineItems(input);
+        const tokens = lineItemsToTokens(items);
+        expect(tokens).toEqual([
+            { count: 1, tag: 'o' },
+            { count: 1, tag: 'o' },
+            { count: 1, tag: 'o' }
+        ]);
+    });
+
+    it('Fill implicit dead cells', () => {
+        const properties = { x: 4, y: 4 };
+        const input = 'o$b2o$2ob!';
+        const items = lineToLineItems(input);
+        const tokens = lineItemsToTokens(items);
+        const matrix = tokensToMatrix({ properties, tokens });
+        expect(matrix).toEqual([
+            [1, 0, 0, 0],
+            [0, 1, 1, 0],
+            [1, 1, 0, 0],
+            [0, 0, 0, 0]
+        ]);
+    });
+
+    it('Fill implicit dead cells - 2', () => {
+        const properties = { x: 4, y: 4 };
+        const input = 'o$b2o$2ob$o!';
+        const items = lineToLineItems(input);
+        const tokens = lineItemsToTokens(items);
+        const matrix = tokensToMatrix({ properties, tokens });
+        expect(matrix).toEqual([
+            [1, 0, 0, 0],
+            [0, 1, 1, 0],
+            [1, 1, 0, 0],
+            [1, 0, 0, 0]
+        ]);
+    });
+});
 
 describe('parsePatternFile', () => {
     it('sould read from file', () => {
@@ -83,7 +171,7 @@ ooo$3b$2ob!
 });
 
 describe('getConfiguration', () => {
-    it('sould should bypass to parseConfigurationString() properly', () => {
+    it('should bypass to parseConfigurationString() properly', () => {
         const config = getConfiguration('glider');
         expect(config).toEqual({
             width: 3,
@@ -92,6 +180,23 @@ describe('getConfiguration', () => {
                 [0, 1, 0],
                 [0, 0, 1],
                 [1, 1, 1]
+            ]
+        });
+    });
+
+    it('should work with laputa', () => {
+        const config = getConfiguration('laputa');
+        expect(config).toEqual({
+            width: 12,
+            height: 7,
+            pattern: [
+                [0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+                [0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+                [0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+                [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+                [1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0],
+                [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0]
             ]
         });
     });
--- src/lib/GameOfLife/rle/parser.ts
+++ src/lib/GameOfLife/rle/parser.ts
@@ -1,26 +1,125 @@
 // Parse pattern files encoded with RLE algorithm as described in the wiki
 // https://conwaylife.com/wiki/Run_Length_Encoded
 
+// Transforms 33ob$ to ['33', 'o', 'b', '$']
+export const lineToLineItems = (line: string) => {
+    return line.split('').reduce((items, char) => {
+        if (items.length === 0) {
+            items.push(char);
+            return items;
+        }
+        if (char.match(/\d/) && items[items.length - 1]?.match(/\d/)) {
+            items[items.length - 1] = items[items.length - 1] + char;
+        } else {
+            items.push(char);
+        }
+        return items;
+    }, [] as string[]);
+};
+
+const TAGS = ['o', 'b', '$', '!'] as const;
+type TagTuple = typeof TAGS;
+type Tag = TagTuple[number];
+type Token = {
+    count: number;
+    tag: Tag;
+};
+export const lineItemsToTokens = (lineItems: string[]): Token[] => {
+    const tokens: Token[] = [];
+
+    if (!lineItems.length) {
+        throw new Error('No line items to tokenize');
+    }
+
+    let currentData: {
+        count?: number;
+        tag?: string;
+    } = {};
+    for (let i = 0; i < lineItems.length; i++) {
+        const currentItem = lineItems[i];
+        const isNumber = currentItem.match(/^\d+$/);
+        if (!isNumber && !['o', 'b', '$', '!'].includes(currentItem)) {
+            throw new Error(`Invalid configuration item ${currentItem}`);
+        }
+
+        if (isNumber) {
+            if (currentData.count) {
+                throw new Error(
+                    `Invalid configuration. Two consecutive numbers ${currentData.count}, ${currentItem}`
+                );
+            }
+            currentData.count = Number(currentItem);
+        } else {
+            if (currentData.tag) {
+                throw new Error(
+                    `Invalid configuration. Something went wrong with tokenization at item ${currentItem}, index ${i}`
+                );
+            }
+            if (!currentData.count) {
+                currentData.count = 1;
+            }
+            if (currentItem === '!') {
+                return tokens;
+            }
+            currentData.tag = currentItem;
+            tokens.push(currentData as Token);
+            currentData = {};
+        }
+    }
+
+    throw new Error('Invalid configuration. No ending character');
+};
+
+export const tokensToMatrix = (params: {
+    properties: { x: number; y: number };
+    tokens: Token[];
+}): number[][] => {
+    const { properties, tokens } = params;
+    const mat: number[][] = [[]];
+
+    while (tokens.length) {
+        const { count, tag } = tokens.shift()!;
+
+        if (tag === '!') {
+            throw new Error('Invalid token. ! should not have been sent here');
+        }
+        if (tag === '$') {
+            for (let i = 0; i < count; i++) {
+                mat.push([]);
+
+                if (mat.length > properties.y) {
+                    throw new Error('Invalid token. Vertical dimensions mismatch');
+                }
+            }
+        } else {
+            for (let i = 0; i < count; i++) {
+                mat[mat.length - 1].push(tag === 'b' ? 0 : 1);
+            }
+        }
+    }
+
+    // Fill missing lines
+    while (mat.length < properties.y) {
+        mat.push(new Array(properties.x).fill(0));
+    }
+
+    // Fill missing columns
+    for (let i = 0; i < mat.length; i++) {
+        while (mat[i].length < properties.x) {
+            mat[i].push(0);
+        }
+    }
+
+    return mat;
+};
+
 type Properties = {
     x: number;
     y: number;
     [k: string]: number | string;
 };
-
-export const parseConfigurationString = (fileAsString: string) => {
-    const lines = fileAsString
-        .replaceAll('\r\n', '\n')
-        .split('\n')
-        .filter((l) => l !== '')
-        .filter((l) => !l.match(/^#/));
-    const propertiesLine = lines.shift();
-    const patternList = lines.join('').split('');
-
-    if (!propertiesLine || !lines) {
-        throw new Error('Invalid pattern file');
-    }
-
-    const properties = propertiesLine
+export const parsePropertiesLines = (line: string): Properties => {
+    const properties = line
         .replaceAll(' ', '')
         .split(',')
         .reduce(
@@ -40,52 +139,27 @@ export const parseConfigurationString = (fileAsString: string) => {
         throw new Error('Invalid pattern file: no dimensions');
     }
 
-    const pattern: number[][] = [[]];
+    return properties;
+};
 
-    let i = 0;
-    while (patternList.length) {
-        const char = patternList.shift();
-        if (!char) {
-            throw new Error('Couldnt parse pattern file');
-        }
+export const parseConfigurationString = (fileAsString: string) => {
+    const lines = fileAsString
+        .replaceAll('\r\n', '\n')
+        .split('\n')
+        .filter((l) => l !== '')
+        .filter((l) => !l.match(/^#/));
+    const propertiesLine = lines.shift();
+    const patternLines = lines.join('');
 
-        if (char.match(/^\d$/)) {
-            let count = char;
-            while (patternList[0].match(/^\d$/) && patternList.length) {
-                count += patternList.shift();
-            }
-            const value = patternList.shift();
-
-            for (let j = 0; j < Number(count); j++) {
-                pattern[pattern.length - 1].push(value === 'b' ? 0 : 1);
-                i++;
-            }
-        } else if (char === '$') {
-            while (pattern[pattern.length - 1].length < properties.x) {
-                pattern[pattern.length - 1].push(0);
-            }
-            pattern.push([]);
-            continue;
-        } else if (char === '!') {
-            if (patternList.length) {
-                throw new Error('Invalid ending char position in pattern file');
-            }
-
-            // Fill the end of the last line if necessary
-            while (pattern[pattern.length - 1].length < properties.x) {
-                pattern[pattern.length - 1].push(0);
-            }
-            // Push enough empty lines if necessary
-            for (let j = i; j < pattern.length; j++) {
-                pattern.push(new Array(properties.x).fill(0));
-            }
-        } else if (['b', 'o'].includes(char)) {
-            pattern[pattern.length - 1].push(char === 'b' ? 0 : 1);
-            i++;
-        } else {
-            throw new Error('Invalid character in pattern file');
-        }
+    if (!propertiesLine || !lines) {
+        throw new Error('Invalid pattern file');
     }
+
+    const properties = parsePropertiesLines(propertiesLine);
+
+    const items = lineToLineItems(patternLines);
+    const tokens = lineItemsToTokens(items);
+    const pattern = tokensToMatrix({ properties, tokens });
 
     return {
         width: properties.x,
