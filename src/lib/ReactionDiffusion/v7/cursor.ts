d4a1783432e316b748b1bde3b87d386490a189cd
Adrien Fabre <adrien.fabre@dashlane.com>
2023-05-03 20:07:05 +0200

    Rework cursor shader structure

--- src/lib/ReactionDiffusion/v7/cursor.ts
+++ src/lib/ReactionDiffusion/v7/cursor.ts
@@ -1,45 +1,15 @@
 import type REGL from 'regl';
 import drawVS from './glsl/draw.vert.glsl';
+import cursorFS from './glsl/cursor.frag.glsl';
 
 let command: REGL.DrawCommand;
 export const initCursorCommand = (regl: REGL.Regl, coloredOutput: REGL.Framebuffer2D) => {
     command = regl({
-        frag: `
-    precision highp float;
-
-    uniform sampler2D prevState;
-    uniform vec2 mousePosition;
-    uniform float penRadius;
-
-    varying vec2 uv;
-
-    void main() {
-        vec4 color = texture2D(prevState, uv);
-
-        float d = distance(uv, mousePosition);
-        if (
-            (d >= penRadius && d <= penRadius + 0.001) ||
-            (uv.x >= mousePosition.x - 0.001 && uv.x <= mousePosition.x + 0.001 && uv.y >= mousePosition.y - 0.01 && uv.y <= mousePosition.y + 0.01) ||
-            (uv.x >= mousePosition.x - 0.01 && uv.x <= mousePosition.x + 0.01 && uv.y >= mousePosition.y - 0.001 && uv.y <= mousePosition.y + 0.001)
-        ) {
-            gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
-        } else {
-            gl_FragColor = color;
-        }
-    }`,
-
+        frag: cursorFS,
         vert: drawVS,
 
         attributes: {
-            position: [
-                [-1, -1],
-                [-1, 1],
-                [1, -1],
-
-                [1, -1],
-                [-1, 1],
-                [1, 1]
-            ]
+            position: [-4, -4, 4, -4, 0, 4]
         },
 
         uniforms: {
@@ -53,7 +23,7 @@ export const initCursorCommand = (regl: REGL.Regl, coloredOutput: REGL.Framebuff
         },
 
         primitive: 'triangles',
-        count: 6
+        count: 3
     });
 };
 
